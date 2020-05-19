dbeed120ed84dc66e02ffd58561c6953a2d814f2
Paweł-Ś <pswiatlon02@gmail.com>
2020-05-19 17:48:24 +0200

    Update css and add mobile css

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  ShowLinks = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  showLinks () {
+    this.ShowLinks = !this.ShowLinks;
   }
 
 }
